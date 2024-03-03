9913f63823c5e8b46208fa1d808f4f6df1a2f9d5
ptduy14 <phantanduy14@gmail.com>
2024-03-03 12:48:07 +0700

    refactor: modify layout version 2

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,6 +4,7 @@ import { NextUIProvider } from "@nextui-org/system";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
 import { Layout } from "../components/layout/layout";
+import { usePathname } from "next/navigation";
 
 export interface ProvidersProps {
   children: React.ReactNode;
@@ -11,12 +12,16 @@ export interface ProvidersProps {
 }
 
 export function Providers({ children, themeProps }: ProvidersProps) {
+  const pathname = usePathname();
+
   return (
     <NextUIProvider>
-      <NextThemesProvider defaultTheme="system" attribute="class" {...themeProps}>
-        <Layout>
-          {children}
-        </Layout>
+      <NextThemesProvider
+        defaultTheme="system"
+        attribute="class"
+        {...themeProps}
+      >
+        {pathname === "/login" ? children : <Layout>{children}</Layout>}
       </NextThemesProvider>
     </NextUIProvider>
   );
