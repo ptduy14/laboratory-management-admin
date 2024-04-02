eabd6de8f83c4bb14c6037f8950892033294e005
ptduy14 <phantanduy14@gmail.com>
2024-04-02 21:59:39 +0700

    feat: add hanlde when token expires to axiosConfig

--- config/axiosConfig.ts
+++ config/axiosConfig.ts
@@ -1,16 +1,22 @@
 import axios from "axios";
+import { redirect } from "next/navigation";
+import { toast } from "react-toastify";
 
-export default function axiosConfig(token = null) {
+export default function axiosConfig(access_token = null) {
   axios.defaults.baseURL = process.env.API_URL;
   axios.interceptors.request.use(
     (config) => {
-      if (token) {
-        config.headers["Authorization"] = "Bearer " + token;
+      if (access_token) {
+        config.headers["Authorization"] = "Bearer " + access_token;
       }
       return config;
     },
     (error) => {
-      console.log(error);
+      if (error.response && error?.response?.data?.statusCode === 401 && access_token) {
+        document.cookie = "next-auth.session-token=; expires=Thu, 01 Jan 1970 00:00:00 UTC;"
+        toast.error("Phiên đăng nhập đã hết hạn !!")
+        redirect("/login");
+      }
       return Promise.reject(error);
     }
   );
