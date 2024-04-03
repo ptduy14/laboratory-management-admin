3cd1fad90de5344bc3659d50b8fb6e6a5c354c64
ptduy14 <phantanduy14@gmail.com>
2024-04-03 22:30:24 +0700

    Revert "fix: add redux/redux-toolkit & persist"
    This reverts commit db38994c60594bd2d00d4211ea9a4a9007e379ab.

--- components/navbar/user-dropdown.tsx
+++ components/navbar/user-dropdown.tsx
@@ -12,22 +12,19 @@ import { DarkModeSwitch } from "./darkmodeswitch";
 import { signOut } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { toast } from "react-toastify";
-import { userSelector } from "@/redux/selector/userSelector";
-import { useDispatch, useSelector } from "react-redux";
-import { removeUser } from "@/redux/slice/userSlice";
+import { useSession } from "next-auth/react";
 
 export const UserDropdown = () => {
   const router = useRouter();
-  const user = useSelector(userSelector);
-  const dispatch = useDispatch();
+  const { data: session } = useSession();
+  console.log(session)
   const handleLogout = async () => {
     await signOut({
-      redirect: false,
+      redirect: false
     });
-    dispatch(removeUser());
     toast.success("Đăng xuất thành công");
     router.push("/login");
-  };
+  }
 
   return (
     <Dropdown>
@@ -37,10 +34,7 @@ export const UserDropdown = () => {
             as="button"
             color="secondary"
             size="md"
-            src={
-              user?.userInfo.photo ||
-              "https://i.pravatar.cc/150?u=a042581f4e29026704d"
-            }
+            src={session?.user.userInfo.photo || "https://i.pravatar.cc/150?u=a042581f4e29026704d"}
           />
         </DropdownTrigger>
       </NavbarItem>
@@ -53,7 +47,7 @@ export const UserDropdown = () => {
           className="flex flex-col justify-start w-full items-start"
         >
           <p>Signed in as</p>
-          <p>{user?.userInfo.email}</p>
+          <p>{session?.user.userInfo.email}</p>
         </DropdownItem>
         <DropdownItem key="settings">My Settings</DropdownItem>
         <DropdownItem key="team_settings">Team Settings</DropdownItem>
@@ -61,12 +55,7 @@ export const UserDropdown = () => {
         <DropdownItem key="system">System</DropdownItem>
         <DropdownItem key="configurations">Configurations</DropdownItem>
         <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
-        <DropdownItem
-          key="logout"
-          color="danger"
-          className="text-danger "
-          onClick={handleLogout}
-        >
+        <DropdownItem key="logout" color="danger" className="text-danger " onClick={handleLogout}>
           Log Out
         </DropdownItem>
         <DropdownItem key="switch">
