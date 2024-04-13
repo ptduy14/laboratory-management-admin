904c37ee949b1cd93ae2ddcc346e09a37c736fa1
ptduy14 <phantanduy14@gmail.com>
2024-04-13 21:36:12 +0700

    style: add input component to detail account

--- components/accounts/add-user.tsx
+++ components/accounts/add-user.tsx
@@ -7,6 +7,7 @@ import {
   ModalFooter,
   ModalHeader,
   useDisclosure,
+  Avatar,
 } from "@nextui-org/react";
 import React from "react";
 
@@ -17,31 +18,81 @@ export const AddUser = () => {
     <div>
       <>
         <Button onPress={onOpen} color="primary">
-          Add User
+          Add Account
         </Button>
         <Modal
           isOpen={isOpen}
           onOpenChange={onOpenChange}
           placement="top-center"
+          size="3xl"
         >
           <ModalContent>
             {(onClose) => (
               <>
                 <ModalHeader className="flex flex-col gap-1">
-                  Add User
+                  Add Account
                 </ModalHeader>
                 <ModalBody>
-                  <Input label="Email" variant="bordered" />
-                  <Input label="First Name" variant="bordered" />
-                  <Input label="Last Name" variant="bordered" />
-                  <Input label="Phone Number" variant="bordered" />
+                  <form className="flex justify-between overflow-y-auto">
+                    <div className="w-1/3 pr-10">
+                      <Avatar
+                        radius="none"
+                        className="w-full h-50"
+                        src={"https://i.pravatar.cc/150?u=a04258114e29026302d"}
+                      />
+                      <div>
+                        <div className="mb-2"></div>
+                        <input
+                          className="block w-full text-xs text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                          aria-describedby="file_input_help"
+                          id="file_input"
+                          type="file"
+                        ></input>
+                        <p
+                          className="mt-1 text-xs text-gray-500 dark:text-gray-300"
+                          id="file_input_help"
+                        >
+                          SVG, PNG, JPG or GIF
+                        </p>
+                      </div>
+                    </div>
 
-                  <Input label="Password" type="password" variant="bordered" />
-                  <Input
-                    label="Confirm Password"
-                    type="password"
-                    variant="bordered"
-                  />
+                    <div className="w-2/3 max-h-80">
+                      <Input
+                        className="mb-7"
+                        label="Email"
+                        variant="bordered"
+                      />
+                      <Input
+                        className="mb-7"
+                        label="First Name"
+                        variant="bordered"
+                      />
+                      <Input
+                        className="mb-7"
+                        label="Last Name"
+                        variant="bordered"
+                      />
+                      <Input
+                        className="mb-7"
+                        label="Phone Number"
+                        variant="bordered"
+                      />
+
+                      <Input
+                        className="mb-7"
+                        label="Password"
+                        type="password"
+                        variant="bordered"
+                      />
+                      <Input
+                        className="mb-7"
+                        label="Confirm Password"
+                        type="password"
+                        variant="bordered"
+                      />
+                    </div>
+                  </form>
                 </ModalBody>
                 <ModalFooter>
                   <Button color="danger" variant="flat" onClick={onClose}>
