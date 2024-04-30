69796514622a9e266da66a43da4b04a8d79ae4a9
ptduy14 <phantanduy14@gmail.com>
2024-04-30 21:51:59 +0700

    fix: incorrect import statement for file

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,6 @@
 import { Login } from "@/components/login/login";
 import { getServerSession } from "next-auth/next"
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import authOptions from "@/lib/authOptions";
 import { redirect } from 'next/navigation'
 
 const login = async () => {
