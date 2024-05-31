b1fcf2230996ecee03ba65ead0472fbd8d1acae5
ptduy14 <phantanduy14@gmail.com>
2024-05-31 17:04:15 +0700

    feat: add metadata for each page && change favicon

--- app/accounts/page.tsx
+++ app/accounts/page.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import {Accounts} from '@/components/accounts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+   title: "Accounts",
+   description: "Laboratory management for CTUT Lab",
+}
 
 const accounts = () => {
    return <Accounts />;
--- app/categories/[id]/resources/page.tsx
+++ app/categories/[id]/resources/page.tsx
@@ -1,4 +1,10 @@
 import { ResourcesFromCategory } from "@/components/category/resources-from-category";
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+   title: "Resources From Category",
+   description: "Laboratory management for CTUT Lab",
+}
 
 const resourcesFromCategory = ({ params }: { params: { id: string } }) => {
   return <ResourcesFromCategory id={params.id}/>;
