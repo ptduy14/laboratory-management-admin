f27f95f86261c1846624de5aa53d241538b3006f
ptduy14 <phantanduy14@gmail.com>
2024-06-07 22:44:24 +0700

    style: modify UI

--- services/categoryService.ts
+++ services/categoryService.ts
@@ -4,7 +4,7 @@ export const CategoryService = {
     getAll: async (url: string) => {
         return await AxiosInstance.get(url);
     },
-    getById: async (id: string) => {
-        return await AxiosInstance.get(`/categories/${id}`);
+    getById: async (url: string) => {
+        return await AxiosInstance.get(url);
     }
 }
