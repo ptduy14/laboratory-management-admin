68c0e16a05791482aa0d2aaadc8fa8fdca1550ef
ptduy14 <phantanduy14@gmail.com>
2024-06-15 00:03:36 +0700

    feat: delete room

---
+++ components/rooms/delete-room.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  useDisclosure,
+  Tooltip,
+} from "@nextui-org/react";
+import { DeleteIcon } from "../icons/table/delete-icon";
+import { RoomService } from "@/services/roomService";
+import { toast } from "react-toastify";
+import { mutate } from "swr";
+
+export const DeleteRoom = ({ roomId }: { roomId: number }) => {
+  const { isOpen, onOpen, onOpenChange, onClose } = useDisclosure();
+
+    const handleDeleteRoom = async () => {
+        const { data } = await RoomService.delete(roomId.toString())
+        mutate(`/rooms`);
+        toast.success("Xóa phòng thí nghiệm thành công")
+        onClose();
+    }
+
+  return (
+    <>
+     <div>
+     <Tooltip content="Xóa phòng thí nghiệm" color="danger">
+        <button onClick={onOpen}>
+          <DeleteIcon size={20} fill="#FF0080" />
+        </button>
+      </Tooltip>
+     </div>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">Xóa phòng thí nghiệm</ModalHeader>
+              <ModalBody>
+                <p>
+                  Bạn có thật sự muốn xóa phòng thí nghiệm này không ?
+                  Hành động này sẽ không thể hoàn tác
+                </p>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="primary" variant="light" onPress={onClose}>
+                  Đóng
+                </Button>
+                <Button color="danger" onClick={handleDeleteRoom}>
+                  Xóa
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
--- services/roomService.ts
+++ services/roomService.ts
@@ -6,10 +6,19 @@ export const RoomService = {
     getAll: async (url : string) => {
         return await AxiosInstance.get(url);
     },
+    getById: async (url: string) => {
+        return await AxiosInstance.get(url);
+    },
+    getResourcesFromRoom: async (url: string) => {
+        return await AxiosInstance.get(url)
+    },
     create: async (payload: AddRoomSchemaType) => {
         return await AxiosInstance.post(`/rooms`, payload)
     },
     update: async (roomId: number, payload: UpdateRoomSchemaType) => {
         return await AxiosInstance.patch(`/rooms/${roomId}`, payload)
+    },
+    delete: async (roomId: string) => {
+        return await AxiosInstance.delete(`/rooms/${roomId}`)
     }
 }
