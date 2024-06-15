4daa059a7af7e79343535c12d4777c9d7d4da8c8
ptduy14 <phantanduy14@gmail.com>
2024-06-16 03:46:04 +0700

    feat: display more info in home page

--- components/home/card-transactions.tsx
+++ components/home/card-transactions.tsx
@@ -1,73 +1,86 @@
-import { Avatar, Card, CardBody } from "@nextui-org/react";
+import {
+  Avatar,
+  Card,
+  CardBody,
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  getKeyValue,
+} from "@nextui-org/react";
 import React from "react";
+import { RoomResourceService } from "@/services/roomResourceService";
+import useSWR from "swr";
+import { ResourcesTransfered } from "../room-resources/room-resources-table/data";
+import { formatDateTime } from "@/utils/formatDateTime";
 
-const items = [
+const columns = [
   {
-    name: "Jose Perez",
-    picture: "https://i.pravatar.cc/150?u=a042581f4e29026024d",
-    amount: "4500 USD",
-    date: "9/20/2021",
+    key: "name",
+    label: "Tên tài nguyên",
   },
   {
-    name: "Jose Perez",
-    picture: "https://i.pravatar.cc/150?u=a042581f4e29026024d",
-    amount: "4500 USD",
-    date: "9/20/2021",
+    key: "quantity",
+    label: "Số lượng",
   },
   {
-    name: "Jose Perez",
-    picture: "https://i.pravatar.cc/150?u=a042581f4e29026024d",
-    amount: "4500 USD",
-    date: "9/20/2021",
-  },
-  {
-    name: "Jose Perez",
-    picture: "https://i.pravatar.cc/150?u=a042581f4e29026024d",
-    amount: "4500 USD",
-    date: "9/20/2021",
-  },
-  {
-    name: "Jose Perez",
-    picture: "https://i.pravatar.cc/150?u=a042581f4e29026024d",
-    amount: "4500 USD",
-    date: "9/20/2021",
+    key: "createdAt",
+    label: "Ngày bàn giao",
   },
 ];
 
+const renderCell = ({ item, columnKey }: { item: ResourcesTransfered; columnKey: any }) => {
+  // @ts-ignore
+  const cellValue = item[columnKey];
+
+  switch (columnKey) {
+    case "createdAt":
+      return formatDateTime(cellValue).formattedDate;
+
+    default:
+      break;
+  }
+
+  return cellValue;
+};
+
 export const CardTransactions = () => {
+  const { data: resourceTransfered, isLoading: isFetchingresourceTransfered } = useSWR(
+    `/room-items?take=5`,
+    async (url) => {
+      const { data } = await RoomResourceService.getAll(url);
+      return data;
+    }
+  );
+
+  if (isFetchingresourceTransfered) return <span>loading</span>;
+
   return (
-    <Card className=" bg-default-50 rounded-xl shadow-md px-3">
-      <CardBody className="py-5 gap-4">
+    <Card className=" bg-default-50 rounded-xl shadow-md">
+      <CardBody className="gap-4">
         <div className="flex gap-2.5 justify-center">
           <div className="flex flex-col border-dashed border-2 border-divider py-2 px-6 rounded-xl">
             <span className="text-default-900 text-xl font-semibold">
-              Latest Transactions
+              Tài nguyên mới bàn giao gần đây
             </span>
           </div>
         </div>
 
         <div className="flex flex-col gap-6 ">
-          {items.map((item, index) => (
-            <div key={index} className="grid grid-cols-4 w-full">
-              <div className="w-full">
-                <Avatar
-                  isBordered
-                  color="secondary"
-                  src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
-                />
-              </div>
-
-              <span className="text-default-900  font-semibold">
-                {item.name}
-              </span>
-              <div>
-                <span className="text-success text-xs">{item.amount}</span>
-              </div>
-              <div>
-                <span className="text-default-500 text-xs">{item.date}</span>
-              </div>
-            </div>
-          ))}
+          <Table aria-label="Example table with dynamic content">
+            <TableHeader columns={columns}>
+              {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+            </TableHeader>
+            <TableBody items={resourceTransfered.data}>
+              {(item: ResourcesTransfered) => (
+                <TableRow key={item.id}>
+                  {(columnKey) => <TableCell>{renderCell({item, columnKey})}</TableCell>}
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
         </div>
       </CardBody>
     </Card>
--- services/roomResourceService.ts
+++ services/roomResourceService.ts
@@ -1,16 +1,19 @@
-import AxiosInstance from "@/config/axiosInstance"
+import AxiosInstance from "@/config/axiosInstance";
 
 export const RoomResourceService = {
-    transferResource: async (payload: any) => {
-       return await AxiosInstance.post(`/room-items/`, payload)
-    },
-    reTransferResource: async (resourceTransferedId: string, payload: any) => {
-        return await AxiosInstance.patch(`/room-items/${resourceTransferedId}`, payload)
-    },
-    getResourceTransferedById: async (url: string) => {
-        return await AxiosInstance.get(url)
-    },
-    delete: async (resourceTransferedId: string) => {
-        return await AxiosInstance.delete(`/room-items/${resourceTransferedId}`)
-     },
-}
+  getAll: async (url: string) => {
+    return await AxiosInstance.get(url);
+  },
+  transferResource: async (payload: any) => {
+    return await AxiosInstance.post(`/room-items/`, payload);
+  },
+  reTransferResource: async (resourceTransferedId: string, payload: any) => {
+    return await AxiosInstance.patch(`/room-items/${resourceTransferedId}`, payload);
+  },
+  getResourceTransferedById: async (url: string) => {
+    return await AxiosInstance.get(url);
+  },
+  delete: async (resourceTransferedId: string) => {
+    return await AxiosInstance.delete(`/room-items/${resourceTransferedId}`);
+  },
+};
