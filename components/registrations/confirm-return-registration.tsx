0e47aeb476d0994680cbba28cb7d53da906ff751
ptduy14 <phantanduy14@gmail.com>
2024-06-29 22:22:47 +0700

    feat: create UI return all resource

--- components/registrations/confirm-return-registration.tsx
+++ components/registrations/confirm-return-registration.tsx
@@ -16,8 +16,8 @@ import { registrationFetcher } from "@/utils/fetchers/registration-fetchers/regi
 import { LoaderSkeletonForm } from "../loader/loader-skeleton-form";
 import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
 import {
-  ReturnResourceRegistrationchemaType,
-  ReturnResourceRegistrationchema,
+  ReturnRegistrationchemaType,
+  ReturnRegistrationchema,
 } from "./schema/return-resource-registration";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useEffect } from "react";
@@ -25,9 +25,44 @@ import { RegistrationService } from "@/services/registrationService";
 import { toast } from "react-toastify";
 import axios from "axios";
 import { ConfirmReturnIcon } from "../icons/table/confirm-return-icon";
+import { fetchAllRegistrationResources } from "@/utils/fetchers/registration-fetchers/registration-resources-fetcher";
+import { RegistrationStatus } from "@/enums/registration-status";
 
 export const ConfirmReturnRegistration = ({ registration }: { registration: Registration }) => {
   const { isOpen, onOpen, onOpenChange, onClose } = useDisclosure();
+  const methods = useForm<ReturnRegistrationchemaType>({
+    resolver: zodResolver(ReturnRegistrationchema),
+  });
+
+  const { data: registrationDetail, isLoading: isFetchingRegistrationDetail } =
+    useSWR<RegistrationDetail>(
+      isOpen ? `/registration/${registration.id}` : null,
+      registrationFetcher
+    );
+
+  const { data: registrationResources, isLoading: isFetchingRegistrationResources } = useSWR(
+    !isFetchingRegistrationDetail ? ["registrationResource", registrationDetail] : null,
+    ([key, registrationDetail]) => fetchAllRegistrationResources(key, registrationDetail)
+  );
+
+  useEffect(() => {
+    if (!isFetchingRegistrationResources && registrationResources && registrationDetail) {
+      methods.reset({
+        uid: registrationDetail.registration.user.id,
+        status: RegistrationStatus.RETURNED,
+        items: registrationResources.map((registrationResource, index) => ({
+          registrationId: registrationDetail.registration.id,
+          itemRegistrationId: registrationResource.item.id,
+          quantity: registrationDetail.items[index].quantity,
+          itemStatus: registrationResource.item.status
+        })),
+      });
+    }
+  }, [isFetchingRegistrationResources]);
+
+  const onSubmit: SubmitHandler<ReturnRegistrationchemaType> = (payload) => {
+    console.log(payload)
+  }
 
   const handleCloseModal = () => {
     onClose();
@@ -37,7 +72,7 @@ export const ConfirmReturnRegistration = ({ registration }: { registration: Regi
     <>
       <Tooltip color="primary" content="Ghi nhận trả">
         <button onClick={onOpen}>
-          <ConfirmReturnIcon/>
+          <ConfirmReturnIcon />
         </button>
       </Tooltip>
       <Modal size="3xl" isOpen={isOpen} onOpenChange={onOpenChange}>
@@ -46,23 +81,28 @@ export const ConfirmReturnRegistration = ({ registration }: { registration: Regi
             <>
               <ModalHeader className="flex flex-col gap-1">Ghi nhận trả</ModalHeader>
               <ModalBody>
-          
+                {!isFetchingRegistrationResources && registrationResources && registrationDetail ? (
                   <form className=" scrollbar scrollbar-thin overflow-y-auto">
                     <div className="w-full max-h-96">
-                      {/* <FormProvider>
-                        <ConfirmReturnRegistrationForm registrationDetail={registration} />
-                      </FormProvider> */}
+                      <FormProvider {...methods}>
+                        <ConfirmReturnRegistrationForm registrationDetail={registrationDetail} registrationResources={registrationResources}/>
+                      </FormProvider>
                     </div>
                   </form>
+                ) : (
+                  <LoaderSkeletonForm />
+                )}
               </ModalBody>
+              {!isFetchingRegistrationResources && registrationResources && registrationDetail ? (
                 <ModalFooter>
-                  <Button color="danger" variant="light" onClick={handleCloseModal}>
+                <Button color="danger" variant="light" onClick={handleCloseModal}>
                     Đóng
                   </Button>
-                  <Button color="primary" onClick={() => console.log()}>
+                  <Button color="primary" onClick={methods.handleSubmit(onSubmit)}>
                     Xác nhận
                   </Button>
                 </ModalFooter>
+              ) : undefined}
             </>
           )}
         </ModalContent>
