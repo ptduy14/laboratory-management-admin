3cc8f812024ac4b96153017afb9e98f462daed81
ptduy14 <phantanduy14@gmail.com>
2024-07-31 21:52:58 +0700

    fix: fix validate create new resource (schema)

--- components/forms/resource-forms/add-resource-common-form.tsx
+++ components/forms/resource-forms/add-resource-common-form.tsx
@@ -85,7 +85,7 @@ export const AddResourceCommonForm = ({
               defaultValue={MeasurementUnit.g}
               id="specifications"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              {...register("specifications")}>
+              {...register("specification")}>
               <option value={MeasurementUnit.g}>{MeasurementUnitNames[MeasurementUnit.g]}</option>
               <option value={MeasurementUnit.kg}>{MeasurementUnitNames[MeasurementUnit.kg]}</option>
               <option value={MeasurementUnit.l}>{MeasurementUnitNames[MeasurementUnit.l]}</option>
